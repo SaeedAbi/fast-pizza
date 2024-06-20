f8de4d460f9d7f64b6e01e30ebd86be16365702b
SaeedAbi <abdary.saeed@gmail.com>
2024-06-20 14:59:25 +0330

    :zap: fetching menu api with react router

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,10 +1,14 @@
 import {getMenu} from "../../services/apiRestaurant.js";
 import {useLoaderData} from "react-router-dom";
+import MenuItem from "./MenuItem.jsx";
 
 function Menu() {
  const menu= useLoaderData()
   console.log(menu)
-  return <h1>Menu</h1>;
+  return <ul>
+    {menu.map(pizza=>(<MenuItem pizza={pizza} key={pizza.id}/>)
+    )}
+  </ul>;
 }
 
 export async function loader(){
