690815d66555ca42938aff19466fe55e68429dfd
SaeedAbi <abdary.saeed@gmail.com>
2024-06-20 14:37:47 +0330

    :construction: add menu components

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,15 @@
+import {getMenu} from "../../services/apiRestaurant.js";
+import {useLoaderData} from "react-router-dom";
+
+function Menu() {
+ const menu= useLoaderData()
+  console.log(menu)
+  return <h1>Menu</h1>;
+}
+
+export async function loader(){
+const menu=await getMenu()
+  return menu
+}
+
+export default Menu;
