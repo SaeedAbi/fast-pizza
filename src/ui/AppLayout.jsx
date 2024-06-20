694f27fbce53eda8c1df35093d0ae73b909cada3
SaeedAbi <abdary.saeed@gmail.com>
2024-06-20 15:10:00 +0330

    :zap: add Loader component for loading and use it for navigation state

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,14 @@
 import Header from "./Header.jsx";
 import CartOverview from "../features/cart/CartOverview.jsx";
-import {Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
+import Loader from "./Loader.jsx";
 
 const AppLayout = () => {
+    const navigation=useNavigate()
+    const isLoading=navigation.state === 'loading'
     return (
-        <div>
+        <div className='layout'>
+            {isLoading && <Loader/>}
             <Header/>
             <main>
                 <Outlet/>
