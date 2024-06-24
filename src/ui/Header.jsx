80fde6d9d5cca667dfdc0d38d2c2250dbc81d3c1
SaeedAbi <abdary.saeed@gmail.com>
2024-06-24 13:28:05 +0330

    :lipstick: styling Header component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,13 @@
 import {Link} from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder.jsx";
+import Username from "../features/users/Username.jsx";
 
 const Header = () => {
     return (
-        <header>
-            <Link to='/'>Fast React Pizza Co.</Link>
+        <header className='bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200 sm:px-6 flex items-center justify-around'>
+            <Link to='/' className='tracking-widest'>Fast React Pizza Co.</Link>
             <SearchOrder/>
-            <p>Saeed</p>
+            <Username/>
         </header>
     );
 };
