35f7e36a285b444344533bca6a22e6aab38a4ec4
SaeedAbi <abdary.saeed@gmail.com>
2024-06-24 13:28:17 +0330

    :lipstick: styling home component

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,11 +1,16 @@
+import CreateUser from "../features/users/CreateUser.jsx";
+
 function Home() {
   return (
-    <div>
-      <h1>
+    <div className='my-10 sm:my-20 text-center px-4'>
+      <h1 className='text-xl text-stone-700 font-semibold mb-8 md:text-3xl'>
         The best pizza.
         <br />
+          <span className='text-yellow-500'>
         Straight out of the oven, straight to you.
+          </span>
       </h1>
+        <CreateUser/>
     </div>
   );
 }
