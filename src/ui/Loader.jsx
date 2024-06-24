b2e38b6d4ef036e5d6c867f71bcd67f7ab75c577
SaeedAbi <abdary.saeed@gmail.com>
2024-06-24 13:28:29 +0330

    :lipstick: styling Loader component

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,8 +1,10 @@
 
 const Loader = () => {
     return (
+        <div className='absolute bg-slate-200/20 backdrop-blur-sm inset-0 flex items-center justify-center'>
         <div className='loader'>
             
+        </div>
         </div>
     );
 };
