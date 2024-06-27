381fbb6940e2cebc5721208342a10b5528d1a482
SaeedAbi <abdary.saeed@gmail.com>
2024-06-27 10:28:53 +0330

    :zap: add onClick to Button component as prop

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import {Link} from "react-router-dom";
 
-const Button = ({children,disabled ,to,type}) => {
+const Button = ({children,disabled ,to,type,onClick}) => {
     const base='disabled:cursor-not-allowed focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 bg-yellow-400 uppercase font-semibold text-stone-800 inline-block text-sm tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300'
 
     const styles={
@@ -9,6 +9,13 @@ const Button = ({children,disabled ,to,type}) => {
         secondary:'px-4 py-2.5 sm:px-6 sm:py-3.5 disabled:cursor-not-allowed focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:ring-offset-2 border-2 border-stone-300 uppercase font-semibold text-sm text-stone-400 inline-block tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300'
     }
     if (to) return <Link to={to} className={styles[type]}>{children}</Link>
+
+    if (onClick) return (
+        <button onClick={onClick} disabled={disabled} className={styles[type]}>
+            {children}
+        </button>
+    );
+
     return (
         <button disabled={disabled} className={styles[type]}>
             {children}
